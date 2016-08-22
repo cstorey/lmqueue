6d5ecbad3b196c73d99640ddc5890b20c7260ba6
Ceri Storey <ceri@lshift.net>
2016-08-22 13:43:20 +0100

    Persist (a) consumer offset.

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,7 @@ const META: &'static str = "meta";
 const DATA: &'static str = "data";
 
 const WRITER_NEXT: &'static str = "writer-next";
+const CONSUMER_NEXT: &'static str = "consumer-next";
 
 pub struct Producer {
     env: Environment,
@@ -36,6 +37,25 @@ fn encode_key(val: u64) -> Result<[u8; 8]> {
     Ok(key)
 }
 
+fn read_offset(meta: &DbHandle, txn: &Transaction, key: &str) -> Result<u64> {
+    let val = {
+        let val = match txn.bind(meta).get(&key) {
+            Ok(val) => val,
+            Err(lmdb_rs::MdbError::NotFound) => 0,
+            Err(e) => return Err(e.into()),
+        };
+        val
+    };
+
+    trace!("{:?} at pos: {:?}", key, val);
+    Ok(val)
+}
+fn write_offset(meta: &DbHandle, txn: &Transaction, key: &str, off: u64) -> Result<()> {
+    try!(txn.bind(meta).set(&key, &off));
+    trace!("{:?} now at pos {:?}", key, off);
+    Ok(())
+}
+
 impl Producer {
     pub fn new<P: AsRef<Path>>(place: P) -> Result<Self> {
         debug!("Producer Open env at: {:?}", place.as_ref());
@@ -51,65 +71,50 @@ impl Producer {
 
     pub fn produce(&mut self, data: &[u8]) -> Result<()> {
         let txn = try!(self.env.new_transaction());
-        let offset = try!(self.read_offset(&txn));
+        let offset = try!(read_offset(&self.meta, &txn, WRITER_NEXT));
         let key = try!(encode_key(offset));
         try!(txn.bind(&self.data).insert(&(&key as &[u8]), &data));
-        debug!("Inserted {:?}bytes", data.len());
-        try!(self.write_offset(&txn, offset + 1));
+        trace!("wrote: {:?}", data);
+        try!(write_offset(&self.meta, &txn, WRITER_NEXT, offset + 1));
         try!(txn.commit());
 
-        Ok(())
-    }
-
-    fn read_offset(&self, txn: &Transaction) -> Result<u64> {
-        let val = {
-            let val = match txn.bind(&self.meta).get(&WRITER_NEXT) {
-                Ok(val) => val,
-                Err(lmdb_rs::MdbError::NotFound) => 0,
-                Err(e) => return Err(e.into()),
-            };
-            val
-        };
-
-        debug!("Producer at pos: {:?}", val);
-        Ok(val)
-    }
-    fn write_offset(&self, txn: &Transaction, off: u64) -> Result<()> {
-        try!(txn.bind(&self.meta).set(&WRITER_NEXT, &off));
-        debug!("Producer now at pos {:?}", off);
         Ok(())
     }
 }
 
 pub struct Consumer {
     env: lmdb_rs::Environment,
-    offset: u64,
+    meta: DbHandle,
+    data: DbHandle,
 }
 
 impl Consumer {
     pub fn new<P: AsRef<Path>>(place: P) -> Result<Self> {
-        let db = try!(EnvBuilder::new().max_dbs(2).open(place.as_ref(), 0o777));
+        let env = try!(EnvBuilder::new().max_dbs(2).open(place.as_ref(), 0o777));
+        let meta = try!(env.create_db(META, DbFlags::empty()));
+        let data = try!(env.create_db(DATA, DbFlags::empty()));
         Ok(Consumer {
-            env: db,
-            offset: 0,
+            env: env,
+            meta: meta,
+            data: data,
         })
     }
 
     pub fn poll(&mut self) -> Result<Option<Vec<u8>>> {
-        let db_handle = try!(self.env.create_db(DATA, DbFlags::empty()));
-
-        let key = try!(encode_key(self.offset));
-
+        let txn = try!(self.env.new_transaction());
+        let offset = try!(read_offset(&self.meta, &txn, CONSUMER_NEXT));
+        let key = try!(encode_key(offset));
         let val = {
-            let txn = try!(self.env.get_reader());
-            let val = match txn.bind(&db_handle).get(&(&key as &[u8])) {
+            let val = match txn.bind(&self.data).get(&(&key as &[u8])) {
                 Ok(val) => Some(val),
                 Err(lmdb_rs::MdbError::NotFound) => None,
                 Err(e) => return Err(e.into()),
             };
             val
         };
-        self.offset += 1;
+        trace!("read @{:?}: {:?}", offset, val);
+        try!(write_offset(&self.meta, &txn, CONSUMER_NEXT, offset + 1));
+        try!(txn.commit());
 
         Ok(val)
     }
